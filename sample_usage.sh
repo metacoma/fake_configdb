069e4e10c1688bc23a000a637ca452c95bc66c90
Sergey Ryabin <sryabin@mirantis.com>
2016-03-21 01:20:13 +0300

    init

---
+++ sample_usage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+
+#create env
+curl -X POST -H 'Content-length: 0' http://localhost:4567/api/v1/config/environment/1
+echo
+curl -X PUT -H 'Content-length: 18' -d "{'data': 'foobar'}" http://localhost:4567/api/v1/config/environment/1/node/1/resource/foobar
+echo
+curl http://localhost:4567/api/v1/config/environment/1/node/1/resource/foobar/values
