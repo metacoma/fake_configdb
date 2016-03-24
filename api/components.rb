aaf9ff5378ef8efbe78aaf33ddf8a996131656aa
Sergey Ryabin <sryabin@mirantis.com>
2016-03-24 20:44:32 +0300

    init

---
+++ api/components.rb
@@ -0,0 +1,20 @@
+class Components < Grape::API
+  resource :components do
+    post do
+      data = JSON.load(request.body.read)
+      begin
+        component = $storage.addComponent(data['name'], data['resource_definitions'])
+        rescue AlreadyExists
+          error!('500 Already exists', 500)
+        rescue ArgumentError
+          error!('500 Argument error', 500)
+        else
+          status 201
+          component
+      end
+    end
+    get do
+      $storage.listComponents()
+    end
+  end
+end
